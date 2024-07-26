23898bc9cb159dd23a218df2dc02009c6258104e
renatakhmyarov <arf@deskit.ru>
2024-07-26 16:30:04 +0300

    0.0.0.1

--- signer/Program.cs
+++ signer/Program.cs
@@ -1,38 +1,59 @@
 ﻿using System.Diagnostics;
-using System.Net;
+using System.Text;
 using System.Text.Json;
 using signer;
 internal class Program
 {
     public static async Task Main(string[] args)
     {
-        var text = File.ReadAllText(args[0]);
+        var text = await File.ReadAllTextAsync(args[0]);
         var signs = JsonSerializer.Deserialize<SignParameters>(text);
-
+        
+        var client = new HttpClient();
         foreach (var sign in signs!.Signs)
         {
-            var client = new HttpClient();
             var response = await client.GetAsync("https://markirovka.crpt.ru/api/v3/true-api/auth/key");
-            if (response.StatusCode != HttpStatusCode.Accepted && response.StatusCode != HttpStatusCode.OK)
+            if (!response.IsSuccessStatusCode)
             {
-                Console.WriteLine(await response.Content.ReadAsStringAsync());
+                await Console.Error.WriteLineAsync("Server https://markirovka.crpt.ru does not response");
                 continue;
             }
      
             var info = JsonSerializer.Deserialize<RequestData>(await response.Content.ReadAsStringAsync());
             if (info==null)
             {
-                Console.WriteLine("No data from https://markirovka.crpt.ru/api/v3/true-api/auth/key");
+                await Console.Error.WriteLineAsync("No data from https://markirovka.crpt.ru/api/v3/true-api/auth/key");
+                continue;
+            }
+            
+            var tempFile = $"{signs.WorkingDirectory}/signData_{sign.Organisation}";
+            await File.WriteAllTextAsync(tempFile, info.Data);
+            //-nochain  
+            var arguments = $"-signf -attached -cadesbes -cert -thumbprint {sign.Thumbprint} -dir {signs.WorkingDirectory} {tempFile}";
+            var proc =Process.Start(signs.CryptcpPath, arguments);
+            await proc.WaitForExitAsync();
+            
+            var requestData = new RequestData()
+            {
+                Uuid = info.Uuid,
+                Data = await File.ReadAllTextAsync($"{tempFile}.sig")
+            };
+            
+            var content = JsonSerializer.Serialize(requestData);
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://markirovka.crpt.ru/api/v3/true-api/auth/simpleSignIn");
+            request.Content = new StringContent(content, Encoding.UTF8, "application/json");
+            
+            response = await client.SendAsync(request);
+            if (!response.IsSuccessStatusCode)
+            {
+                await Console.Error.WriteLineAsync(await response.Content.ReadAsStringAsync());
                 continue;
             }
 
-            var tempFile = Path.GetTempFileName();
-            //var tempFile = signs.WorkingDirectory + @"/test.txt";
-            await File.WriteAllTextAsync(tempFile, info.Data);
-            
-            var arguments = $"-signf -nochain -attached -thumbprint {sign.Thumbprint} -dir {signs.WorkingDirectory} {tempFile}";
-            var proc =Process.Start(signs.CryptcpPath, arguments);
-            await proc.WaitForExitAsync();
+            var testString = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(testString);
         }
+        
+        client.Dispose();
     }
 }
